664a1c4bce954151618986af6c5396ee30d007b9
Lightop <lightop@yandex.ru>
2017-01-05 18:33:02 +0300

    Input now is kinda working

--- otto.py
+++ otto.py
@@ -8,60 +8,66 @@ import socket
 
 class OttoShell(cmd.Cmd):
 
+	# def __init__(self):
+	# 	self.universe = 1
+	# 	self.ip_address = "192.168.2.100"
+	# 	self.port = 9000
+
+
+
+
+
 	def do_run (self, arg):
-		oscoutput = udp_client.SimpleUDPClient(ip_address, port)
-		universe = 1
-		wrapper = ClientWrapper()
-		client = wrapper.Client()
-		client.RegisterUniverse(universe, client.REGISTER, NewData)
-		wrapper.Run()
+
+		def NewData(data):
+		  value = 0
+		  for x in range(10):
+		  	address = "/0/chan/%d"%x
+		  	percents = int (data[x]/255*100)
+		  	if percents == 0:
+		  		value = "-"
+		  	
+		  	elif percents >=100:
+		  		value = "FL"
+		  	
+		  	else:
+		  		value = percents
+		  	
+		  	self.oscoutput.send_message(address, value)
+
+		self.oscoutput = udp_client.SimpleUDPClient(self.ip_address, self.port)
+		self.universe =1
+		self.wrapper = ClientWrapper()
+		self.client = self.wrapper.Client()
+		self.client.RegisterUniverse(self.universe, self.client.REGISTER, NewData)
+		print ("xscfsdfvcs")
+		self.wrapper.Run()
+		print("csdvdvdsvcsdvcsdcvsdcccccccc")
 
 	def do_ip (self, arg):
 		print (arg)
 		try:
 			socket.inet_aton(arg)
-			ip_address = arg
-			print (ip_address)
+			self.ip_address = arg
+			print (self.ip_address)
 		except socket.error:
 			print ("Not an address")
 
 	def do_port (self,arg):
 		try:
 			arg = int(arg)
+			self.port = arg
 		except ValueError:
 			print("Ой!")
 
 	def do_status(self,arg):
-		print (ip_address)
-		print (port)
+		print (self.ip_address)
+		print (self.port)
 
 	def do_exit(self,arg):
 		print ("До свидания!")
 		return True
 
-
-
-
-		
-ip_address = "192.168.1.124"
-port = 9000
-
-
-def NewData(data):
-  value = 0
-  for x in range(10):
-  	address = "/0/chan/%d"%x
-  	percents = int (data[x]/255*100)
-  	if percents == 0:
-  		value = "-"
-  	
-  	elif percents >=100:
-  		value = "FL"
-  	
-  	else:
-  		value = percents
-  	
-  	oscoutput.send_message(address, value)
 
 
 cmd = OttoShell()
